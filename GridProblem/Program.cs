38cbbf7336f25abfed06f1efddde4bb5f908dc3e
Alex <76208677+azapalac300@users.noreply.github.com>
2026-04-15 15:27:39 -0700

    Initial Commit

---
+++ GridProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GridProblem;
+
+Console.WriteLine("Hello, World!");
+Solution s = new Solution();
+
+/*
+while (true)
+{
+
+}*/
---
+++ GridProblem/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GridProblem
+{
+    public class Solution
+    {
+        public List<String> gridPositions;
+        public Solution()
+        {
+            string path = "grid_input.txt";
+            gridPositions = new List<String>();
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists! Path: " + path);
+                StreamReader reader = new StreamReader(path);
+                string line = "";
+
+                while ((line = reader.ReadLine())!= null)
+                {
+                    gridPositions.Add(line);
+                    Console.WriteLine(line);
+                }
+
+                
+            }
+            else
+            {
+                Console.WriteLine("File could not be found!");
+            }
+        }
+            
+     
+
+        public void Solve () { }
+        public static void Foo()
+        {
+            Console.WriteLine("This is Foo");
+        }
+    }
+}
