fa5974142359fc635e0a9bda64f20941b4f3b5b6
Alex <76208677+azapalac300@users.noreply.github.com>
2026-04-19 08:57:01 -0700

    Found solution
    Only works for odd numbered grids

--- GridProblem/Program.cs
+++ GridProblem/Program.cs
@@ -2,7 +2,7 @@
 using GridProblem;
 
 Solution s = new Solution();
-
+s.Solve();
 /*
 while (true)
 {
--- GridProblem/Solution.cs
+++ GridProblem/Solution.cs
@@ -4,19 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 //using 
 
 namespace GridProblem
 {
+    public class Point
+    {
+        public float X, Y;
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float Distance(Point p)
+        {
+            Point diff = new Point(X - p.X, Y-p.Y);
+            return diff.Magnitude();
+        }
+
+        public float Magnitude()
+        {
+            return MathF.Sqrt(X * X + Y * Y);
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+
+        public static Point operator +(Point p1, Point p2) { 
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+    }
     public class Solution
     {
-        public List<float[]> gridPositions;
+        public List<Point> gridPositions;
 
 
         public Solution()
         {
             string path = "grid_input.txt";
-            gridPositions = new List<float[]>();
+            gridPositions = new List<Point>();
 
             if (File.Exists(path))
             {
@@ -28,12 +64,10 @@ namespace GridProblem
 
                     //Console.WriteLine(line);
                     string[] words = line.Split(',');
-                    float [] position = new float[]{
-                    float.Parse(words[0]),
-                    float.Parse(words[1]),
-                    };
-                    Console.WriteLine(position[0] + "," + position[1]);
-                    gridPositions.Add(position);
+                    float x = float.Parse(words[0]);
+                    Point p = new Point(float.Parse(words[0]), float.Parse(words[1]));
+                    Console.WriteLine(p.X + "," + p.Y);
+                    gridPositions.Add(p);
                 }
 
             }
@@ -41,81 +75,78 @@ namespace GridProblem
             {
                 Console.WriteLine("File could not be found!");
             }
+
+           
         }
 
 
         //Key assumptions:
         //Assume that smallest square side is the shortest distance between two given points on the same axis
         //We know all cells are square so this will give the length of both sides
-        //Assume alpha is small.
+
         //ALL graph points are present in the given data set.
-        //This means we need to find the smallest delta X and delta Y such that deltaX == deltaY. Once we find that and the origin
-        //we know where to start by finding the minimum magnitude from 0,0. This is the first point of the graph.
+        //This means we need to find the smallest delta X and use it to compute delta Y (Assuming the grid is square and each cell is square)
+        //This gives us the dimensions of an idndividual cell
+        //We find the mid point and use that as an "Anchor point" to re compute the data using our delta x and delta Y
+        //Once we have matched all the data we use the coefficients to find the row and column of each data point
 
         public void Solve()
         {
-            float alpha = 0.0f;
+            //Get the average of all the points. This will give us the center of the graph
+            float xAvg = gridPositions.Average(p => p.X);
+            float yAvg = gridPositions.Average(p => p.Y);
+            Point midPointApprox = new Point(xAvg, yAvg);
 
-
-            //Find an arbitrary point in the graph. For this we will be using the 0 index.
-            //From here, find the shortest magnitude vector difference from this point to any
-
-            int startingIndex = 0;
-
-
-            int closestPointIndex = 0;
-            float minMag = float.MaxValue; //Stores the minimum magnitude
-            float[] origin = gridPositions[startingIndex];
-            //Try to find the minimum magnitude difference vector and its 90 degree counterpart
-            //This will give us our cell dimensions
+            Console.WriteLine("Mid point approximate: " + midPointApprox);
+            float minDist = float.MaxValue;
+            int centerIndex = -1;
+            Point midPoint = new Point(0, 0);
+            //The point in the dataset closest to the average will be the center point
             for (int i = 0; i < gridPositions.Count; i++)
             {
+                Point diff =  gridPositions[i] - midPointApprox;
+                float dist = diff.Magnitude();
 
-                if (i != startingIndex)
+                if (dist < minDist)
                 {
-                    float[] pos = gridPositions[i];
-                    float diffX = pos[0] - origin[0];
-                    float diffY = pos[1] - origin[1];
-
-                    float mag = MathF.Sqrt(diffX * diffX + diffY * diffY);
-
-                    if (mag < minMag)
-                    {
-                        minMag = mag;
-                        closestPointIndex = i;
-                    }
-
-
+                    minDist = dist;
+                    centerIndex = i;
                 }
             }
+            midPoint = gridPositions[centerIndex];
+            Console.WriteLine("Mid Point: " + midPoint);
 
-            float[] closestPoint = gridPositions[closestPointIndex];
-            //Extract the cell size by subtracting the closest point from the origin. This will give us one of the "walls" of the cell.
-            float[] deltaA = new float[]
-            {
-                origin[0] - closestPoint[0],
-                origin[1] - closestPoint[1],
-            };
-            //create the perpendicular complement of DeltaA
-            float[] deltaB = new float[] {
-                -deltaA[1],
-                deltaA[0]
-            };
+            //Now we find the smallest diff that isn't the mid point. Since every cell of the grid is a square, this will be our first vector
+            //We just need to rotate it 90 degrees to find the secod
+            //we can re-use minDist here, no need to declare a new variable
 
-
-            //loop through all points again. This time we will find how far away they are from the origin in units of DeltaA and DeltaB
-            //(OgX, OgY) + A*(DeltaA) + B*(DeltaB) = gridPos
-            //DeltaA and DeltaB are whole numbers
-            //A*(DeltaA) + B*(DeltaB) = gridPos - origin
-            //Need to find A and B
             
-            for (int i = 0; i < gridPositions.Count; i++)
+            minDist = float.MaxValue;
+            Point deltaA = new Point(0, 0);
+            for(int i = 0; i < gridPositions.Count; i++)
             {
-
-
-
+                if(i != centerIndex)
+                {
+                    Point diff = midPoint - gridPositions[i];
+                    float dist = diff.Magnitude();
+                    if (dist < minDist)
+                    {
+                        minDist= dist;
+                        deltaA = diff;
+                    }
+                }
             }
+            //Now that we have delta A, we can find delta b
+            Point deltaB = new Point (-deltaA.Y, deltaA.X);
+
+
+            //Assuming the graph will always be a perfect square, we can find its dimensions
+            int dim = (int)MathF.Sqrt(gridPositions.Count);
+
+            //since we are use integer division 
+
         }
     
     }
 }
+    
